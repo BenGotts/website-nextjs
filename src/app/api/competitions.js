c048046e9c276148d4e33d6c2c73b1b5cc40db82
BenGotts <837gb1@gmail.com>
2024-01-24 16:32:40 -0800

    Change caching for competitions

--- src/app/api/competitions.js
+++ src/app/api/competitions.js
@@ -1,7 +1,5 @@
 export default async function getCompetitions() {
-    return await fetch('https://www.worldcubeassociation.org/api/v0/users/6836?upcoming_competitions=true', {
-        cache: 'force-cache'
-    })
+    return await fetch('https://www.worldcubeassociation.org/api/v0/users/6836?upcoming_competitions=true', { next: { revalidate: 3600 } })
       .then(response => response.json())
       .then(data => { return data["upcoming_competitions"] })
       .catch(err => { console.error(err); return []});
