a89546e875e6910b85c5555aec3f8063da9d03fe
BenGotts <837gb1@gmail.com>
2024-03-06 15:50:47 -0800

    Fix competition cache and cards

--- src/app/competitions/Competitions.jsx
+++ src/app/competitions/Competitions.jsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { Card, CardBody, CardHeader, CardFooter, Divider, Link } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, CardFooter, Divider} from "@nextui-org/react";
 import "@cubing/icons";
 import ReactCountryFlag from "react-country-flag";
+import Link from "next/link";
 
 export default function Competitions({ competitions }) {
     const currentDate = new Date()
@@ -17,39 +18,44 @@ export default function Competitions({ competitions }) {
         <>
             {competitions ? 
             competitions.map((comp) => (
-                <Card
+                <Link href={comp.url} 
+                      target="_blank"
                       key={comp.id}
-                      fullWidth
-                      onPress={() => window.open(comp.url, '_blank')}
-                      >
-                    <CardHeader className="m-2">
-                        <div className="flex h-5 items-center space-x-2">
-                            <div className="text-lg">{comp.name}</div>
-                            <Divider orientation="vertical" />
-                            <div>{comp.city}</div>
-                            <ReactCountryFlag countryCode={comp.country_iso2} svg className="text-xl" />
-                        </div>
-                    </CardHeader>
-                    <Divider />
-                    <CardBody className="m-2">
-                    <p>{comp.date_range}</p>
-
-                        <div>{comp.event_ids.map((id) => (
-                            <span key={id} className={`m-1 text-lg cubing-icon event-${id}`}></span>
-                        ))}</div>
-                    </CardBody>
-                    {isCompetitionLive(comp.start_date, comp.end_date) && (<Divider />)}
-                    {isCompetitionLive(comp.start_date, comp.end_date) && (
-                    <CardFooter>
-                        <Link
-                        isExternal
-                        showAnchorIcon
-                        href={`https://live.worldcubeassociation.org/link/competitions/${comp.id}`}
+                    >
+                    <Card
+                        className="m-2"
+                        fullWidth
+                        isHoverable
                         >
-                        <p className="text-red-500 m-1">[Live]</p><p>View results here</p>
-                        </Link>
-                    </CardFooter>)}
-                </Card>
+                        <CardHeader className="m-2">
+                            <div className="flex h-5 items-center space-x-2">
+                                <div className="text-lg">{comp.name}</div>
+                                <Divider orientation="vertical" />
+                                <div>{comp.city}</div>
+                                <ReactCountryFlag countryCode={comp.country_iso2} svg className="text-xl" />
+                            </div>
+                        </CardHeader>
+                        <Divider />
+                        <CardBody className="m-2">
+                        <p>{comp.date_range}</p>
+
+                            <div>{comp.event_ids.map((id) => (
+                                <span key={id} className={`m-1 text-lg cubing-icon event-${id}`}></span>
+                            ))}</div>
+                        </CardBody>
+                        {isCompetitionLive(comp.start_date, comp.end_date) && (<Divider />)}
+                        {isCompetitionLive(comp.start_date, comp.end_date) && (
+                        <CardFooter>
+                            <Link
+                            isExternal
+                            showAnchorIcon
+                            href={`https://live.worldcubeassociation.org/link/competitions/${comp.id}`}
+                            >
+                            <p className="text-red-500 m-1">[Live]</p><p>View results here</p>
+                            </Link>
+                        </CardFooter>)}
+                    </Card>
+                </Link>
             ))
             : <div>No upcoming competitions</div>}
         </>
