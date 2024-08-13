4c5ca7686c17c7a2bef736a5995119aac942ae75
BenGotts <837gb1@gmail.com>
2024-08-12 20:37:16 -0700

    Remove revalidate from fetch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 }
 
 export default async function Home() {
-  const videoId = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=1&playlistId=UUY4J5vw3Ed8Rc3Njc-2qzfg&key=${process.env.YOUTUBE_API_KEY}`, { next: { revalidate: 3600 }})
+  const videoId = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=1&playlistId=UUY4J5vw3Ed8Rc3Njc-2qzfg&key=${process.env.YOUTUBE_API_KEY}`)
   .then(response => response.json())
   .then(data => { return data['items'][0]['contentDetails']['videoId'] })
   .catch(err => console.error(err))
