da9f23631249fc89e70f2449e003f46dce95243c
BenGotts <837gb1@gmail.com>
2025-09-04 21:21:19 -0700

    Remove past competitions for Vercel timeout

--- src/app/competitions/page.jsx
+++ src/app/competitions/page.jsx
@@ -1,8 +1,8 @@
 import OngoingCompetitions from "@/components/competitions/OngoingCompetitions";
 import UpcomingCompetitions from "@/components/competitions/UpcomingCompetitions"
-import PastCompetitions from "@/components/competitions/PastCompetitions"
+// import PastCompetitions from "@/components/competitions/PastCompetitions"
 import { WCA_ID } from "@/consts";
-import { Separator } from "@/components/ui/separator";
+// import { Separator } from "@/components/ui/separator";
 
 export const revalidate = 3600;
 export const metadata = {
@@ -11,30 +11,34 @@ export const metadata = {
 }
 
 export default async function CompetitionsPage() {
-  const userInfoPromise = fetch(`https://www.worldcubeassociation.org/api/v0/users/${WCA_ID}?upcoming_competitions=true&ongoing_competitions=true`)
+  const userInfo = await fetch(`https://www.worldcubeassociation.org/api/v0/users/${WCA_ID}?upcoming_competitions=true&ongoing_competitions=true`)
   .then(response => response.json());
 
-  const pastCompetitionsPromise = fetch(`https://www.worldcubeassociation.org/api/v0/persons/${WCA_ID}/competitions`)
-  .then(response => response.json());
+  
+  // const userInfoPromise = fetch(`https://www.worldcubeassociation.org/api/v0/users/${WCA_ID}?upcoming_competitions=true&ongoing_competitions=true`)
+  // .then(response => response.json());
 
-  const [userInfo, pastCompetitionsData] = await Promise.all([
-    userInfoPromise,
-    pastCompetitionsPromise
-  ]).catch(err => {
-    console.error(err);
-    return [ { upcoming_competitions: [], ongoing_competitions: [] }, [] ];
-  });
+  // const pastCompetitionsPromise = fetch(`https://www.worldcubeassociation.org/api/v0/persons/${WCA_ID}/competitions`)
+  // .then(response => response.json());
+
+  // const [userInfo, pastCompetitionsData] = await Promise.all([
+  //   userInfoPromise,
+  //   pastCompetitionsPromise
+  // ]).catch(err => {
+  //   console.error(err);
+  //   return [ { upcoming_competitions: [], ongoing_competitions: [] }, [] ];
+  // });
 
   const upcomingCompetitions = userInfo.upcoming_competitions.sort((a, b) => a.start_date.localeCompare(b.start_date) || a.end_date.localeCompare(b.end_date));
   const ongoingCompetitions = userInfo.ongoing_competitions.sort((a, b) => a.start_date.localeCompare(b.start_date) || a.end_date.localeCompare(b.end_date));
-  const pastCompetitions = pastCompetitionsData.sort((a, b) => b.start_date.localeCompare(a.start_date));
+  // const pastCompetitions = pastCompetitionsData.sort((a, b) => b.start_date.localeCompare(a.start_date));
 
   return (
     <div className="m-2 sm:m-4 space-y-3 sm:space-y-4 flex-grow">
       <OngoingCompetitions competitions={ongoingCompetitions} />
       <UpcomingCompetitions competitions={upcomingCompetitions} />
-      <Separator className="my-2 sm:my-4" />
-      <PastCompetitions competitions={pastCompetitions} />
+      {/* <Separator className="my-2 sm:my-4" />
+      <PastCompetitions competitions={pastCompetitions} /> */}
     </div>
   )
 }
